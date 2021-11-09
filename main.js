99bb9c4fd12b820369e5b815df7b86eda5bde101
Bennett <bjupfield@gmail.com>
2021-11-09 12:15:54 -0600

    done!

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+function init(){
+  const glyph = [...document.getElementsByClassName("like-glyph")];
+  for(num in glyph){
+    console.log(glyph[num].parentNode.parentNode.parentNode.parentNode);
+    glyph[num].addEventListener("click",(obj)=>{
+      const node = obj.target;
+      if(node.className === "like-glyph activated-heart"){
+        node.textContent = EMPTY_HEART;
+        node.classList.remove("activated-heart")
+      }
+      else{
+        console.log("heppening")
+        mimicServerCall()
+        .then(()=>{
+          console.log("not failed")
+          console.log(node.textContent)
+          node.textContent = FULL_HEART;
+          console.log(node.textContent)
+          node.classList.add("activated-heart");
+        })
+        .catch(()=>{
+          console.log("failed")
+          const err = document.getElementById("modal");
+          err.classList.remove("hidden");
+          setTimeout(()=>{
+            err.classList.add("hidden")
+        },3000)
+      })
+      }
+    });
+  }  
+}
+window.addEventListener("DOMContentLoaded", init)
 
 
 
